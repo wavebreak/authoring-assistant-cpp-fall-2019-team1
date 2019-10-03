bc6a2262055570bfd0415f19354048c3ac1b857e
tccmobile <wsmith@tulsacc.edu>
2019-10-02 20:15:03 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  
+//
+//  Created by William Smith on 10/2/19.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+   /* Type your code here. */
+
+   return 0;
+}
